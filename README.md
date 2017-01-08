586f6df6ff93ea4f44f009711f80a103e23f4714
Filippo Valsorda <filippo@cloudflare.com>
2017-01-08 14:47:44 -0500

    Add main binary

--- README.md
+++ README.md
@@ -4,9 +4,27 @@
 
 ```
 $ go get -u github.com/FiloSottile/zcash-mini
+$ zcash-mini
+###############################################################
+#
+#  Here is your new z-address
+#
+#      zcZvkjKo24G8LzSiU9UEvPeeu6rCYJc46bhe5dsYLqNgcR12GFCQsE6Z6w4LH3Mb82aYoGgpjRpK8VcwTesaFbpPZhmkCJe
+#
+#  and here is the secret key
+#
+#      SKxoocXqMjxsiE3DdEguWhcbqf3vz45rpMdmVqXPwzReBBDhXj6v
+#
+#  KEEP IT SAFE, IT HAS NOT BEEN SAVED ANYWHERE
+#
+#  To spend, import it with
+#
+#      zcash-cli z_importkey KEY rescan
+#
+###############################################################
 ```
 
-**This is experimental software and comes with no promises of not eating your money.**
+**This is experimental software and it will eat your money, your hard drive and your pets.**
 
 ## Use cases
 
@@ -17,9 +35,8 @@ $ go get -u github.com/FiloSottile/zcash-mini
 ## Features
 
 * offline generation of z-address wallets
-* paper backup generation
-* private key export
-* X LoC, easy to review
+* paper backup generation (TODO)
+* 150 LoC, easy to review
 * pure Go, easy to cross-compile / deploy
 
 Balance management and spending operations are not supported.
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/FiloSottile/zcash-mini/zcash"
+)
+
+var logo = `
+
+             :88SX@888@@X8:
+          %%Xt%tt%SSSSS:XXXt@@
+        @S;;tt%%%t    ;;::XXXXSX
+      .t:::;;%8888    88888tXXXX8;
+     .%...:::8             8::XXX%;
+     8888...:t888888X     8t;;::XX8
+    %888888...:::;:8    :Xttt;;;::X@
+    888888888...:St    8:%%tttt;;;:X
+    88888888888S8    :%;ttt%%tttt;;X
+    %888888888%t    8S:;;;tt%%%ttt;8
+     8t8888888     S8888888Stt%%%t@
+     .@tt888@              8;;ttt@;
+      .8ttt8@SSSSS    SXXXX%:;;;X;
+        X8ttt8888%    %88...::X8
+          %8@tt88;8888%8888%8X
+             :@888@XXX@888:
+
+                    _       _
+          _ __ ___ (_)_ __ (_)
+         | '_ \ _ \| | '_ \| |
+         | | | | | | | | | | |
+         |_| |_| |_|_|_| |_|_|
+
+`
+
+var template = `%s
+
+###############################################################
+#
+#  Here is your new z-address
+#
+#      %s
+#
+#  and here is the secret key
+#
+#      %s
+#
+#  KEEP IT SAFE, IT HAS NOT BEEN SAVED ANYWHERE
+#
+#  To spend, import it with
+#
+#      zcash-cli z_importkey KEY rescan
+#
+###############################################################
+
+`
+
+func main() {
+	rawKey := zcash.GenerateKey()
+	rawAddr, err := zcash.KeyToAddress(rawKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key := zcash.Base58Encode(rawKey, zcash.ProdSpendingKey)
+	addr := zcash.Base58Encode(rawAddr, zcash.ProdAddress)
+
+	fmt.Printf(template, logo, addr, key)
+}
